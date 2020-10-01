0fdde01822a9ed538a3addeefed110a853a52b0a
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2020-10-01 14:35:00 -0500

    Add more test code

--- WhatsMyName/Program.vb
+++ WhatsMyName/Program.vb
@@ -1,7 +1,31 @@
 Imports System
 
 Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+'    Sub Main(args As String())
+'        Console.WriteLine("Hello World!")
+'    End Sub
+
+'    Sub Main(args as String())
+'        Console.WriteLine(vbCrLf + "What is your name? ")
+'        Dim name = Console.ReadLine()
+'        Dim currentDate = DateTime.Now
+'        Console.WriteLine($"{vbCrLf}Hello, {name}, on {currentDate:d} at {currentDate:t}!")
+'        Console.Write(vbCrLf + "Press any key to exit... ")
+'        Console.ReadKey(True)
+'    End Sub
+
+    Private _num1 As Integer
+    Private _num2 As Integer
+    Private _answer As Integer
+    Sub Main()
+        Console.WriteLine("Type a number and press Enter")
+        _num1 = Console.ReadLine()
+        Console.WriteLine("Type another number to add to it and press Enter")
+        _num2 = Console.ReadLine()
+        _answer = _num1 + _num2
+        Console.WriteLine("The answer is " & _answer)
+'        Console.ReadLine()
+        Console.Write(vbCrLf + "Press any key to exit... ")
+        Console.ReadKey(True)
     End Sub
 End Module
