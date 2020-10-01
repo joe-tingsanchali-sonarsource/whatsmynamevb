c24def05854cdc5a1d3d55c29e82b72f8076de46
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2020-10-01 16:40:06 -0500

    Added more non-compliant code for vbnet analysis but its not working still...

--- WhatsMyName/Program.vb
+++ WhatsMyName/Program.vb
@@ -1,3 +1,5 @@
+Option Base On
+Option Explicit On
 Imports System
 
 Module Program
@@ -17,6 +19,20 @@ Module Program
     Private _num1 As Integer
     Private _num2 As Integer
     Private _answer As Integer
+    Public Class Sample
+        Private Const Condition As Boolean = 3
+
+        Public Sub MySub()
+            If Condition Then
+                Exit Sub                  ' Noncompliant
+            End If
+        End Sub
+        Function MyFunction() As Object
+            ' ...
+            MyFunction = 42
+            Exit Function              ' Noncompliant
+        End Function
+    End Class
     Sub Main()
         Console.WriteLine("Type a number and press Enter")
         _num1 = Console.ReadLine()
@@ -28,6 +44,8 @@ Module Program
         Console.Write(vbCrLf + "Press any key to exit... ")
         Console.ReadKey(True)
 
+        ' This is non-compliant code for VB.NET, not VB!
+        ' But this isn't working for https://rules.sonarsource.com/vbnet/type/Code%20Smell/RSPEC-2340
         Dim i = 1
 
         Do
@@ -38,7 +56,11 @@ Module Program
             i = i + 1
         Loop
     End Sub
+End Module
 
+' This is non-compliant code for VB.NET, not VB!
+' See https://rules.sonarsource.com/vbnet/type/Code%20Smell/RSPEC-2368
+Module Module1
     Sub WriteMatrix(ByVal matrix As Integer()())
         ' ...
     End Sub
